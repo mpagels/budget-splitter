5feca27e86db4cd7b741f3f868c2534e07ebe110
Martin <office@martinpagels.de>
2022-06-04 00:22:19 +0200

    add super simple dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,63 @@
+import styled from "styled-components";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to budget-splitter</h1>
-    </div>
+    <HomeWrapper>
+      <SpendList>
+        <SpendItem>
+          <Infos>
+            <h2>Tanken</h2>
+            <Money isMe={false}>25,00 €</Money>
+          </Infos>
+          <p>28.6</p>
+        </SpendItem>
+        <SpendItem>
+          <Infos>
+            <h2>Abendessen für alle</h2>
+            <Money isMe>45,00 €</Money>
+          </Infos>
+          <p>28.6</p>
+        </SpendItem>
+      </SpendList>
+      <Footer>
+        <CalculatedBudget>20€</CalculatedBudget>
+      </Footer>
+    </HomeWrapper>
   );
 }
+
+const HomeWrapper = styled.main`
+  height: 100vh;
+`;
+
+const Infos = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Footer = styled.footer`
+  position: fixed;
+  bottom: 0;
+  border-top: 2px solid black;
+  width: 100vw;
+`;
+
+const CalculatedBudget = styled.p`
+  text-align: center;
+  color: green;
+  font-size: 2em;
+`;
+
+const Money = styled.p`
+  color: ${({ isMe }) => (isMe ? "green" : "red")};
+`;
+
+const SpendItem = styled.li`
+  border: 1px solid black;
+  margin: 5px 0;
+`;
+
+const SpendList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+`;
