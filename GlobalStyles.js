9fe169a15dcb28e8e410d3106cb2f60f93728709
Martin <office@martinpagels.de>
2022-06-04 23:43:34 +0200

    add GlobalStyles to project

---
+++ GlobalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+html,
+body {
+    padding: 0;
+    margin: 0;
+}
+
+a {
+    color: inherit;
+    text-decoration: none;
+}
+
+* {
+    box-sizing: border-box;
+}
+`;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
+import { GlobalStyles } from "../GlobalStyles";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
