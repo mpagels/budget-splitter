fb8b4b504e868734be893b6f6f4b0a612335fd9c
Martin <office@martinpagels.de>
2022-06-06 11:46:31 +0200

    second test green

--- utils/calculateSplits.js
+++ utils/calculateSplits.js
@@ -1,18 +1,34 @@
-/* {
-    splits: [
-      {
-        id: 1,
-        nameValue: 'martin',
-        number: 70,
-        moneyValue: 'together',
-      },
-    ],
-  }, */
+/* splits: [
+    {
+      id: 1,
+      nameValue: 'martin',
+      number: 40,
+      moneyValue: 'martin',
+    },
+    {
+      id: 2,
+      nameValue: 'jana',
+      number: 20,
+      moneyValue: 'jana',
+    },
+    {
+      id: 3,
+      nameValue: 'lene',
+      number: 10,
+      moneyValue: 'together',
+    },
+  ], */
 
 export default function calculateSplits(splits, account) {
   return splits.reduce((prev, cur) => {
     if (cur.moneyValue === 'together') {
       return prev + cur.number / 2
     }
+
+    if (cur.moneyValue === account) {
+      return prev + cur.number
+    }
+
+    return prev
   }, 0)
 }
